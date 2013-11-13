e000a30c857c5b626d54efec96c49f128c242956
angelinay0403 <angelinay0403@gmail.com>
2013-11-13 14:22:13 -0500

    Conditional_worksheet assigment.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+// Angelina Castro, 11/13/2013, Conditionals_Worksheet.
+
+//Stuff your face 1
+// if the contestant weigh 250 pounds or more, the contestant quallify for the contest.
+	var competitorWeigh = 260;
+
+// if contestant weigh is less than 250 pounds
+if(competitorWeigh < 250){
+ 	// if is less than 250 pounds
+ 	console.log ("the competitor will not qualify!");
+}else{
+	//if is greater or equal than 250 pounds.
+	console.log ("the competitor will qualify!");
+}
+
+
+//last chance for gas!
+var carMPG = 25 // miles per gallon.
+var carReadingTank = 50 // in percent.
+var carGascapacity = 12 // gallons
