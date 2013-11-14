f542b645b843f5d1500b1bd01c23e83293e874d9
angelinay0403 <angelinay0403@gmail.com>
2013-11-14 13:28:33 -0500

    fixing errors

--- js/script.js
+++ js/script.js
@@ -34,7 +34,12 @@ if("carMPG * carGascapacity > 200"){
 
 // Check The Login.
 
-var usernameEnter = joe19
+var usernameEnter = joe1916
 var passwordEnter = Joe1918
 var correctUsername = Joe1919
 var correctPassword = joe1918
+
+if(usernameEnter == correctUsername && passwordEnter == correctPassword){
+	// if the user name enter is the same as the correct username and the password enter is the same as correct password
+	console.log("Welcome Joe1919!") // this is going to print out.
+}
